bd57e9fa73735d7bbbd74162082066a367f5696e
Paulino Torres <paulino.t.j@hotmail.com>
2021-09-14 09:26:03 -0500

    Update functions.js

--- zeldio/functions.js
+++ zeldio/functions.js
@@ -16,26 +16,3 @@ function keyPressed() {
     } 
   }
 }
-
- /* 
-function keyPressed() {
-  if (keyCode === 109) {
-    if (ocultarDev === true) {
-      document.getElementById("uiDev").style.display = "none";
-      ocultarDev = false;
-    } else {
-      document.getElementById("uiDev").style.display = "block";
-      ocultarDev = true;
-    }
-  }
-  if (keyCode === 189) {
-    if (ocultarDev === true) {
-      document.getElementById("uiDev").style.display = "none";
-      ocultarDev = false;
-    } else {
-      document.getElementById("uiDev").style.display = "block";
-      ocultarDev = true;
-    }
-  }
-}
- */
